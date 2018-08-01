471063874de2a8f934f28950fca656a45ecdcebe
teena123inapp <teena.aa@inapp.com>
2018-08-01 22:56:01 +0530

    updated

---
+++ updated/tutor/tutorial/accounts/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from .import views
+from django.contrib.auth.views import login,logout
+
+
+urlpatterns = [
+
+    url(r'^login/$',login,{'template_name':'accounts/login.html'}),
+    url(r'^logout/$',logout,{'template_name':'accounts/logout.html'}),
+    url(r'^register/$',views.register,name='register'),
+    url(r'^profile/$',views.view_profile,name='view_profile'),
+    url(r'^profile/edit/$',views.edit_profile,name='edit_profile'),
+    url(r'^change-password/$', views.change_password, name='change_password')
+
+]
